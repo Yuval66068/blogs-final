886b571f337f537e8914342e88a7a1095683e626
Yuval66068 <tamiry68@gmail.com>
2024-11-24 20:46:45 +0200

    hamburger manu created

--- client/src/components/navBar/NavBar.tsx
+++ client/src/components/navBar/NavBar.tsx
@@ -3,135 +3,99 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import { NavLink } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
 import classes from "./NavBar.module.scss";
 import Box from "@mui/material/Box";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext, AuthContextType } from "../../context/AuthContext";
 import Avatar from "@mui/material/Avatar";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
 
 const Navbar = () => {
-  const { auth, logout, currentUser } = useContext(
-    AuthContext
-  ) as AuthContextType;
+  const { auth, logout, currentUser } = useContext(AuthContext) as AuthContextType;
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
+  const navLinks = [
+    { to: "/", label: "Home" },
+    { to: "/myBlogs", label: "My Blogs", authRequired: true },
+    { to: "/favorites", label: "Favorites", authRequired: true },
+    { to: "/newBlog", label: "New Blog", authRequired: true },
+    { to: "/login", label: "Logout", onClick: logout, authRequired: true },
+    { to: "/login", label: "Login", authRequired: false },
+    { to: "/signup", label: "Signup", authRequired: false },
+  ];
+
+  const renderNavLinks = (isDrawer = false) =>
+    navLinks
+      .filter(link => (auth ? true : !link.authRequired))
+      .map(({ to, label, onClick }, index) => (
+        <NavLink
+          key={index}
+          to={to}
+          className={({ isActive }) =>
+            `${classes.navLink} ${isActive ? classes.active : ""}`
+          }
+          onClick={onClick}
+          style={isDrawer ? { textDecoration: "none", color: "inherit" } : {}}
+        >
+          {isDrawer ? (
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary={label} />
+              </ListItemButton>
+            </ListItem>
+          ) : (
+            label
+          )}
+        </NavLink>
+      ));
 
   return (
     <AppBar position="static" className={classes.navBarContainer}>
       <Toolbar>
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          {/* <MenuIcon /> */}
-          {auth && currentUser && (
-            <>
-              <Avatar alt="Remy Sharp" src={currentUser.image.url} />
-              <Typography variant="h6" component="p" sx={{ flexGrow: 1 }}>
-                {`Welcome ${currentUser.name.first} ${currentUser.name.last}`}
-              </Typography>
-            </>
-          )}
-        </IconButton>
-        {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          BlogApp
-        </Typography> */}
-        <Box className={classes.mainNav}>
-          <NavLink
-            to="/"
-            className={({ isActive, isPending }) =>
-              isPending
-                ? `${classes.pending} ${classes.navLink}`
-                : isActive
-                ? `${classes.active} ${classes.navLink}`
-                : classes.navLink
-            }
-          >
-            Home
-          </NavLink>
-
-          {auth ? (
-            <>
-              <NavLink
-                to="/myBlogs"
-                className={({ isActive, isPending }) =>
-                  isPending
-                    ? `${classes.pending} ${classes.navLink}`
-                    : isActive
-                    ? `${classes.active} ${classes.navLink}`
-                    : classes.navLink
-                }
-              >
-                My Blogs
-              </NavLink>
-              <NavLink
-                to="/favorites"
-                className={({ isActive, isPending }) =>
-                  isPending
-                    ? `${classes.pending} ${classes.navLink}`
-                    : isActive
-                    ? `${classes.active} ${classes.navLink}`
-                    : classes.navLink
-                }
-              >
-                Favorites
-              </NavLink>
-              <NavLink
-                to="/newBlog"
-                className={({ isActive, isPending }) =>
-                  isPending
-                    ? `${classes.pending} ${classes.navLink}`
-                    : isActive
-                    ? `${classes.active} ${classes.navLink}`
-                    : classes.navLink
-                }
-              >
-                New Blog
-              </NavLink>
-              <NavLink
-                to="/login"
-                className={({ isActive, isPending }) =>
-                  isPending
-                    ? `${classes.pending} ${classes.navLink}`
-                    : isActive
-                    ? `${classes.active} ${classes.navLink}`
-                    : classes.navLink
-                }
-                onClick={logout}
-              >
-                Logout
-              </NavLink>
-            </>
-          ) : (
-            <>
-              <NavLink
-                to="/login"
-                className={({ isActive, isPending }) =>
-                  isPending
-                    ? `${classes.pending} ${classes.navLink}`
-                    : isActive
-                    ? `${classes.active} ${classes.navLink}`
-                    : classes.navLink
-                }
-              >
-                Login
-              </NavLink>
-              <NavLink
-                to="/signup"
-                className={({ isActive, isPending }) =>
-                  isPending
-                    ? `${classes.pending} ${classes.navLink}`
-                    : isActive
-                    ? `${classes.active} ${classes.navLink}`
-                    : classes.navLink
-                }
-              >
-                Signup
-              </NavLink>
-            </>
-          )}
-        </Box>
+        {isSmallScreen ? (
+          <>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={() => setDrawerOpen(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Drawer
+              anchor="left"
+              open={isDrawerOpen}
+              onClose={() => setDrawerOpen(false)}
+            >
+              <Box sx={{ width: 250 }}>
+                <List>{renderNavLinks(true)}</List>
+              </Box>
+            </Drawer>
+          </>
+        ) : (
+          <>
+            {auth && currentUser && (
+              <Box sx={{ display: "flex", alignItems: "center", mr: 2 }}>
+                <Avatar alt="User Avatar" src={currentUser.image.url} />
+                <Typography variant="h6" sx={{ ml: 1 }}>
+                  {`Welcome ${currentUser.name.first} ${currentUser.name.last}`}
+                </Typography>
+              </Box>
+            )}
+            <Box className={classes.mainNav} sx={{ display: "flex", flexGrow: 1 }}>
+              {renderNavLinks()}
+            </Box>
+          </>
+        )}
       </Toolbar>
     </AppBar>
   );
--- client/src/pages/favorites/Favorites.tsx
+++ client/src/pages/favorites/Favorites.tsx
@@ -1,10 +1,39 @@
+import { Box, Container, Typography } from "@mui/material";
 import classes from "./Favorites.module.scss";
+import { useContext, useEffect } from "react";
+import { AuthContext, AuthContextType } from "../../context/AuthContext";
+import { BlogContext, BlogContextType } from "../../context/BlogContext";
+import HomeBlog from "../home/HomeBlog";
+
 
 const Favorites = () => {
+  const {blogs, getAllBlogs} = useContext(BlogContext) as BlogContextType;
+  const {currentUser} = useContext(AuthContext) as AuthContextType;
+  
+  const favorites = blogs?.filter((blog) => blog.likes.includes(currentUser?._id as string));
+  
+  useEffect(() => {
+    getAllBlogs();
+  }, []);
+
   return (
-    <div>
-      favorites
-    </div>
+    <Container maxWidth="lg" className={classes.favoritesContainer}>
+      <Typography variant="h3" align="center" gutterBottom>
+        Blog Home
+      </Typography>
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+          gap: 2,
+          marginTop: 4,
+        }}
+      >
+        {favorites?.map((blog) => (
+         <HomeBlog key={blog._id} blog={blog}/>
+        ))}
+      </Box>
+    </Container>
   )
 }
 
